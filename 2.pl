c806acac0dbf90984c7543a5d0639b4c607885e3
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2019-02-26 11:16:35 +0100

    Added nnodes

--- 2.pl
+++ 2.pl
@@ -3,4 +3,10 @@ isBinaryTree(node(X, Y)) :-
     isBinaryTree(X),
     isBinaryTree(Y).
 
+nnodes(leaf, N) :-
+    N = 1.
+nnodes(node(X,Y), N) :-
+    nnodes(X, Res1),
+    nnodes(Y,Res2),
+    N is 1 + Res1 + Res2.
 
