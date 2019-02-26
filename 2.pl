1d0ce6b4cebbdb53d79fc7185458aaee97aab668
hl <h.m.nguyen@student.ru.nl>
2019-02-26 13:29:04 +0100

    added 2

--- 2.pl
+++ 2.pl
@@ -1,12 +1,15 @@
-isBinaryTree(leaf).
-isBinaryTree(node(X, Y)) :-
-    isBinaryTree(X),
-    isBinaryTree(Y).
+isBinaryTree((leaf, N)).
+isBinaryTree(node((X,A),(Y,B))) :-
+    isBinaryTree((X,A)),
+    isBinaryTree((Y,B)).
 
-nnodes(leaf, N) :-
+nnodes( (leaf,A), N) :-
     N = 1.
-nnodes(node(X,Y), N) :-
-    nnodes(X, Res1),
-    nnodes(Y,Res2),
+nnodes(node((X,A), (Y,B)), N) :-
+    nnodes( (X,A) , Res1),
+    nnodes( (Y,B) ,Res2),
     N is 1 + Res1 + Res2.
 
+makeBinary(X,leaf):-
+	X < 1,
+	leaf is (leaf,0).
