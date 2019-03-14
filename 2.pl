7feddae9c686c527086bdff8fca3dec027ce3130
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2019-03-14 11:30:09 +0100

    remove unneeded makeBinary

--- 2.pl
+++ 2.pl
@@ -4,27 +4,23 @@ isBinaryTree(node((X,A),(Y,B))) :-
     isBinaryTree((X,A)),
     isBinaryTree((Y,B)).
 
-nnodes( (leaf,_), N) :-
+nnodes(leaf, N) :-
     N = 1.
 nnodes(node((X,A), (Y,B)), N) :-
     nnodes( (X,A) , Res1),
     nnodes( (Y,B) ,Res2),
     N is 1 + Res1 + Res2.
 
-
-makeBinary(N, Tree):-
-	N < 0,
-	Tree = false.
 makeBinary(N,Tree):-
 	N = 0,
 	Tree = leaf(N).
 makeBinary(N, Tree):-
 	N > 0,
-        NewN is N-1,
-        makeBinary(NewN, R1),
-        Tree = node(R1, R1, N).
+    NewN is N-1,
+    makeBinary(NewN, R1),
+    Tree = node(R1, R1, N).
 	
 makeTree(N,NumberOfChildren,Tree):-
 	N>0,
 	NumberOfChildren>0,
-	Tree = true.
+    Tree = true. 
