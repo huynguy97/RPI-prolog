d47cc08b10a130062e4364d519bb25f9e331f181
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2019-03-20 14:00:33 +0100

    Added makeTree

--- 2.pl
+++ 2.pl
@@ -8,26 +8,34 @@ nnodes(leaf(_), N) :-
     N = 1.
 nnodes(node((X,A), (Y,B)), N) :-
     nnodes( (X,A) , Res1),
-    nnodes( (Y,B) ,Res2),
+    nnodes( (Y,B) , Res2),
     N is 1 + Res1 + Res2.
 
-makeBinary(N, Tree):-
-	N = 0,
-	Tree = leaf(N).
-makeBinary(N, Tree):-
-	N > 0,
+makeBinary(N, Tree) :-
+    N = 0,
+    Tree = leaf(N).
+makeBinary(N, Tree) :-
+    N > 0,
     NewN is N-1,
     makeBinary(NewN, R1),
     Tree = node(R1, R1, N).
-	
-makeTree(N,NumberOfChildren,Tree):-
-	N>0,
-	NumberOfChildren>0,
-        Tree = true. 
 
-createNode( [Head | Tails], NumberOfChildren, List ):-
-	NumberOfChildren = 0,
-	List = [].
-	
-        
+makeTree(N, _, Tree) :-
+    N = 0,
+    Tree = leaf.
+makeTree(N, NumberOfChildren, Tree) :-
+    N > 0,
+    NewN is N-1,
+    makeTree(NewN, NumberOfChildren, R1),
+    createLayer(R1, NumberOfChildren, R2),
+    Tree = node(R2).
 
+createLayer(_, N, Result) :-
+    N = 0,
+    Result = [].
+
+createLayer(A, N, Result) :-
+    N > 0,
+    NewN is N-1,
+    createLayer(A, NewN, TempResult),
+    append([A], TempResult, Result).
