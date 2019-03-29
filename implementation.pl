655af1a12c5263b1838ba022c48a1492ba9b2587
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2019-03-29 18:13:48 +0100

    Added checkhittingset

--- implementation.pl
+++ implementation.pl
@@ -1,6 +1,20 @@
-makeHittingTree
+/**/
 
+ 
 
+checkSingleHSet(_, []).
+checkSingleHSet( [Head | Tail] , List):-
+    member(Head, List);
+    checkSingleHSet(Tail, List).
 
+checkAllHSets(_, []).
+checkAllHSets(A, [B | C]):-
+    checkSingleHSet(A, B),
+    checkAllHSets(A, C).
 
+labelNode( CS , node(A,B,C), Return ):-
+    append(A, CS, T),
+    Return = node(T, B, C).
 
+makeHittingTree( [ A | B ] , Tree).
+    
