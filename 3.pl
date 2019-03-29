e915d3666a8d0faaf5ebb00b98d82d7caed3c058
Huy Nguyen <huy.nguy97@gmail.com>
2019-03-29 16:16:04 +0100

    test

--- 3.pl
+++ 3.pl
@@ -10,7 +10,12 @@ our_subset([Sub|RestSub],[Set|RestSet]):-
 	member(Sub, [Set|RestSet]),
 	our_subset(RestSub, [Set|RestSet]).
 
-path(From, From, _, _,)Path):-
-	path = [From,From].
+path(From, From, _, Visited, Path):-
+	append(Visited, [From],List),
+	Path = List.
+path(From, To, [edge(A,B) | Edges], Visited, Path):-
+	member(edge(X,Y),[edge(A,B)|Edges]),
+	not(member(Y,Visited)),
+	append(Visited, [X], NewVisited),
+	path(B,To, Edges, NewVisited, Path).
 
-path
