41c05a10109e039db5ea39bc382da5be1159997b
Jeroen Wijenbergh <jeroenwijenbergh@protonmail.com>
2019-04-06 13:18:38 +0200

    not not needed

--- implementation.pl
+++ implementation.pl
@@ -18,7 +18,6 @@ makeHittingTree(SD, COMP, OBS, [node(_, Path, Predecessor) | OtherNodes], Tree) 
     makeHittingTree(SD, COMP, OBS, NewNodes, NewTree),
     append([node(CS, Path, Predecessor)], NewTree, Tree).
 makeHittingTree(SD, COMP, OBS, [node(_, Path, Predecessor) | OtherNodes], Tree) :-
-    not(tp(SD, COMP, OBS, Path, _)),
     makeHittingTree(SD, COMP, OBS, OtherNodes, NewTree), % continue with the rest of the nodes.
     append([leaf(Path, Predecessor)], NewTree, Tree). % leaf doesn't have CS because it is always empty
 
@@ -28,5 +27,4 @@ gatherDiagnoses([FirstElement | RestTree] , Diagnoses) :-
     gatherDiagnoses(RestTree, NewDiagnoses),
     append([X], NewDiagnoses, Diagnoses).
 gatherDiagnoses([FirstElement | RestTree], Diagnoses) :-
-    not(FirstElement = leaf(_, _)),
     gatherDiagnoses(RestTree, Diagnoses).	
